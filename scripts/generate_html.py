19c3ca6c1b30df749da181b3a8b6325002d6b92f
Sason <sasonreza@gmail.com>
2022-01-05 18:19:24 -0800

    Create generate_html.py

--- scripts/generate_html.py
+++ scripts/generate_html.py
@@ -1,1 +1,35 @@
+"""Search all sub-directories/sub-folders for files named "article.md".
+
+Invoke `article.py` on found `article.md` files to convert the .md files to HTML pages.
+"""
 # Search all sub-directories for files name "article.md"
+from os import walk as os_walk
+from os.path import join as os_path_join
+
+
+def search_folders() -> list:
+    article_paths = []
+
+    # Nested loops are undesirable, but the iteration space is small enough to ignore performance.
+    for dirpath, dirnames, filenames in os_walk("../blog/"):
+        for file in filenames:
+            if file == "article.md":
+                article = os_path_join(dirpath, file)
+
+                if "index.html" in filenames:
+                    # Ignore article.md files that have already been converted to HTML files.
+                    # Ideally, we'd like to compare hashes to see if the article's contents differ
+                    # from the HTML file, and update the HTML file accordingly.
+                    # However, the above solution would overwrite manual edits made to the HTML.
+                    # We'll ignore articles with existing HTML files in the meantime.
+                    print("\t[-] Ignoring article, found index.html")
+                else:
+                    print(f"[+] Found article in dir {dirpath}: {article}")
+                    article_paths.append(article)
+
+    return article_paths
+
+
+if __name__ == "__main__":
+    articles = search_folders()
+    # TODO: Invoke article.py for all files in articles
---
+++ second_post/article.md
@@ -0,0 +1,31 @@
+# Sample Header
+
+Sample text. More sample text. Even more sample text.
+
+Sample text on a newline.
+
+> Sample quote
+>> Sample nested quote
+>>> Sample double-nested quote
+
+```python
+import sample_import
+from random import choice
+
+some_var = "Hello World!"
+
+def some_func(some_param: int) -> str:
+    return str(some_param)
+
+print(some_func(some_var))
+```
+
+---
+## Sample sub-header
+
+Sample text under a sub-header.
+
+[Sample URL, don't click me.](www.fars.io/) Or do click it. It's safe.
+
+![Sample Image](bean_icon.png "Sample image title")
+
