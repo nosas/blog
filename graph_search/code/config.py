423f34b5b7f151b5fb2c4e26f4f7f5e9dc250e3a
Sason B <nosas@users.noreply.github.com>
2022-01-30 01:38:08 -0800

    Create game config file

---
+++ graph_search/code/config.py
@@ -0,0 +1,22 @@
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# Game window settings
+WIDTH = 1024
+HEIGHT = 768
+
+# Game settings
+FPS = 100
+TITLE = "Agent Movement Demo"
+BG_COLOR = BLACK
+
+# Map settings
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
