653dcfe91bc04a193403da00b5b4db49e6cfd166
Sason B <nosas@users.noreply.github.com>
2022-05-23 19:33:17 -0700

    Create functions for plots, gifs, and predictions

--- deep_learning_python/ch3/code/linear_classifier.py
+++ deep_learning_python/ch3/code/linear_classifier.py
@@ -1,7 +1,10 @@
 # %% Import libraries
-import tensorflow as tf
+from io import BytesIO
+
+import imageio
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
 
 # %% Create 500 cleanly separated data points for each class
 num_samples_per_class = 500
@@ -25,6 +28,7 @@ labels = np.vstack(
         np.ones((num_samples_per_class, 1), dtype=np.float32),
     )
 )
+
 
 # %% Plot the two classes
 # Class A is represented by green dots, and class B is represented by blue dots,
@@ -88,16 +92,18 @@ def training_step(inputs, targets, learning_rate: float = 0.1) -> float:
 num_epochs = 50
 loss_all = []
 predictions_all = []
+parameters_all = []
 
 for step in range(num_epochs):
     step_loss = training_step(inputs, labels)
     loss_all.append(step_loss)
-    if step % 1 == 0:
+    if step % 5 == 0:
         print(f"Step {step}: Loss = {step_loss:.4f}")
     # Save every prediction
     predictions_all.append(model(inputs))
+    parameters_all.append((W.numpy(), b.numpy()))
 
-# Retrieve the model's final predictions
+# %% Retrieve the model's final predictions
 predictions = predictions_all[-1]
 
 # %% Plot the loss over time
@@ -108,18 +114,48 @@ plt.ylabel("Loss")
 plt.savefig("../img/loss_exclude_initial.png", transparent=False)
 
 
+# %% Function to plot the two classes
+def plot_prediction(
+    prediction,
+    inputs,
+    parameters: tuple[np.ndarray, float] = None,
+    legend: bool = False,
+):
+    plt.scatter(
+        inputs[:num_samples_per_class, 0],
+        inputs[:num_samples_per_class, 1],
+        c=[
+            "green" if pred else "blue"
+            for pred in prediction[:num_samples_per_class, 0] < 0.5
+        ],
+        label="Class A",
+    )
+    plt.scatter(
+        inputs[num_samples_per_class:, 0],
+        inputs[num_samples_per_class:, 1],
+        c=[
+            "green" if pred else "blue"
+            for pred in prediction[num_samples_per_class:, 0] < 0.5
+        ],
+        label="Class B",
+    )
+
+    if parameters:
+        W, b = parameters
+        x = np.linspace(-1, 5, 100)
+        y = -W[0] / W[1] * x + (0.5 - b) / W[1]
+        plt.plot(x, y, c="red")
+        plt.xlim(-3, 6)
+        plt.ylim(-3, 6)
+    if legend:
+        plt.legend(["Class A", "Class B"])
+
+
 # %% Plot the line separating the two classes based on the model's final predictions
 # The line is represented by the equation y = -W[0]/W[1] * x - (b/W[1])
-x = np.linspace(-2, 6, 100)
-y = -W[0] / W[1] * x - (0.5 - b) / W[1]
-plt.plot(x, y, c="red")
-plt.scatter(
-    inputs[:, 0],
-    inputs[:, 1],
-    c=["green" if pred else "blue" for pred in predictions[:, 0] < 0.5],
-)
+plot_prediction(predictions, inputs, parameters=(W, b), legend=True)
 
-# %% Plot the line for predictions in predictions_all, each prediction is a subplot
+# %% Plot the model's first ten predictions
 plt.figure(0)
 for i, prediction in enumerate(predictions_all[:10]):
 
@@ -148,40 +184,105 @@ for i, prediction in enumerate(predictions_all[:10]):
         label="Class B",
         edgecolors="none",
     )
-    # ax.scatter(
-    #     inputs[:, 0],
-    #     inputs[:, 1],
-    #     c=["green" if pred else "blue" for pred in prediction[:, 0] < 0.5],
-    # )
-    # ax.plot(x, y, c="red")
-plt.legend()
+
 plt.show()
 
-# %% Scatter plot for the model's first prediction where the dots are green if the prediction is accurate, red if the prediction is incorrect
-plt.figure(1)
-plt.scatter(
-    inputs[:, 0],
-    inputs[:, 1],
-    c=[
-        "green" if labels[idx] == pred else "red"
-        for idx, pred in enumerate(predictions_all[0] > 0.5)
-    ],
-)
-plt.show()
+
+# %% Scatter plot for the model's predictions where the dots are green if the prediction is accurate, red if the prediction is incorrect
+def plot_prediction_acc(
+    prediction: np.ndarray,
+    inputs: np.ndarray,
+    buffer=None,
+    parameters: tuple[np.ndarray, float] = None,
+    savename: str = "",
+    title: str = "",
+):
+    plt.scatter(
+        inputs[:, 0],
+        inputs[:, 1],
+        c=[
+            "green" if labels[idx] == pred else "red"
+            for idx, pred in enumerate(prediction > 0.5)
+        ],
+        alpha=0.20,
+        s=100,
+    )
+    if parameters:
+        W, b = parameters
+        x = np.linspace(-1, 5, 100)
+        y = -W[0] / W[1] * x + (0.5 - b) / W[1]
+        plt.plot(x, y, c="red")
+        plt.xlim(-3, 6)
+        plt.ylim(-3, 6)
+    if title:
+        plt.title(title)
+    if savename:
+        plt.savefig(f"../img/{savename}.png", transparent=False)
+    if buffer:
+        plt.savefig(buffer, format="png", transparent=False)
+    else:
+        plt.show()
+    plt.close()
+
+
+# %% Create function for making gif
+def make_gif(predictions: np.ndarray, inputs: np.ndarray, savename: str):
+    fig, ax = plt.subplots()
+
+    with imageio.get_writer(f"../img/{savename}.gif", mode="I") as writer:
+        # for prediction_idx in [0, 1, 2, -1]:
+        for prediction_idx, prediction in enumerate(predictions):
+            params = parameters_all[prediction_idx]
+            buffer = BytesIO()
+            plot_prediction_acc(
+                prediction=prediction,
+                inputs=inputs,
+                buffer=buffer,
+                parameters=params,
+                title=f"Prediction {prediction_idx}",
+            )
+            buffer.seek(0)
+            img = plt.imread(buffer, format="png")
+            writer.append_data(img)
+
+    plt.show()
+
+
+def make_gif_with_duration(
+    predictions: np.ndarray, inputs: np.ndarray, savename: str, duration: float
+):
+    images = []
+    for prediction_idx, prediction in enumerate(predictions):
+        params = parameters_all[prediction_idx]
+        buffer = BytesIO()
+        plot_prediction_acc(
+            prediction=prediction,
+            inputs=inputs,
+            buffer=buffer,
+            parameters=params,
+            title=f"Prediction {prediction_idx}",
+        )
+        buffer.seek(0)
+        images.append(plt.imread(buffer, format="png"))
+    imageio.mimsave(f"../img/{savename}.gif", images, duration=duration)
+
+
+# %% Make gif
+make_gif(predictions_all, inputs, "prediction_accuracy")
 
 # %% Train the model for another 50 epochs
-# loss_all = []
 for step in range(num_epochs):
     step_loss = training_step(inputs, labels)
     loss_all.append(step_loss)
     if step % 5 == 0:
-        print(f"Step {step}: Loss = {step_loss}")
+        print(f"Step {step}: Loss = {step_loss:.4f}")
+    # Save every prediction
+    predictions_all.append(model(inputs))
+    parameters_all.append((W.numpy(), b.numpy()))
 
 # %% Plot the loss over time
 plt.clf()
-plt.plot(
-    loss_all[num_epochs:]
-)  # Skip the first value, since it's just the initialization
+plt.plot(loss_all[1:])  # Skip the first value, since it's just the initialization
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
 plt.show()
@@ -190,25 +291,16 @@ plt.show()
 predictions = model(inputs)
 
 # %% Plot the line separating the two classes
-y = -W[0] / W[1] * x - (0.5 - b) / W[1]
-plt.plot(x, y, c="red")
-plt.scatter(
-    inputs[:, 0],
-    inputs[:, 1],
-    c=["green" if pred else "blue" for pred in predictions[:, 0] < 0.5],
-)
+plot_prediction(predictions, inputs, parameters=(W, b))
 
 # %% Scatter plot for the model's last prediction where the dots are green if the prediction is accurate, red if the prediction is incorrect
-plt.figure(1)
+plot_prediction_acc(predictions, inputs)
 
-plt.scatter(
-    inputs[:, 0],
-    inputs[:, 1],
-    c=[
-        "green" if labels[idx] == pred else "red"
-        for idx, pred in enumerate(predictions > 0.5)
-    ],
-)
-plt.show()
+# %% Plot all predictions as a gif
+make_gif(predictions_all, inputs, "prediction_accuracy_100")
 
-# %%
+# %% Make gif of first 20 predictions with 1s duration
+make_gif_with_duration(predictions_all[:20], inputs, "prediction_accuracy_slowed", 0.5)
+
+# %% Plot the model's classes with params
+plot_prediction(predictions, inputs, parameters=(W, b), legend=True)
