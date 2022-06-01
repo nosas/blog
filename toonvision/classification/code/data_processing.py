de43c1a6da7850de7c205f77cdf240c183c38008
Sason B <nosas@users.noreply.github.com>
2022-05-31 19:25:06 -0700

    Add check for .xml files during image processing

--- toonvision/classification/code/data_processing.py
+++ toonvision/classification/code/data_processing.py
@@ -4,7 +4,7 @@ from img_utils import (
     extract_objects_from_img,
     save_objects_to_img,
 )
-from os import rename
+from os import rename, path
 from glob import glob
 
 # %% Test functions with
@@ -13,6 +13,7 @@ RAW_DIR = IMG_DIR + "/raw"
 DATA_DIR = IMG_DIR + "/data"
 UNSORTED_DIR = IMG_DIR + "/unsorted"
 PROCESSED_DIR = RAW_DIR + "/processed"
+SCREENSHOTS_DIR = RAW_DIR + "/screenshots"
 
 
 def verify_folder_structure():
@@ -46,18 +47,30 @@ def verify_folder_structure():
 
 
 # %% Convert raw images to data images
-def process_images(raw_images_dir: str = RAW_DIR, image_type: str = "png") -> None:
-    for img_path in glob(f"{raw_images_dir}/*.{image_type}"):
+def process_images(
+    raw_images_dir: str = SCREENSHOTS_DIR,
+    image_type: str = "png",
+    move_images: bool = False,
+) -> None:
+    screenshots = glob(f"{raw_images_dir}/*/*.{image_type}")
+    print(f"Found {len(screenshots)} screenshots")
+    for img_path in screenshots:
         print(f"Processing {img_path}")
-        xml_path = img_path.replace(".png", ".xml")
-        # Extract objects' bounding box dimensions from XML
-        objs_from_xml = extract_objects_from_xml(xml_path)
-        # Extract objects from images
-        objs_from_img = extract_objects_from_img(img_path, objs_from_xml)
-        # Save extracted objects to images
-        save_objects_to_img(objs_from_img, UNSORTED_DIR)
-        # Move raw image to processed directory
-        for f in [img_path, xml_path]:
-            new_path = f.replace(RAW_DIR, PROCESSED_DIR)
-            print(f"    Moving {f} to {new_path}")
-            rename(f, new_path)
+        xml_path = img_path.replace(f".{image_type}", ".xml")
+        if path.exists(xml_path):
+            # Extract objects' labels and bounding box dimensions from XML
+            objs_from_xml = extract_objects_from_xml(xml_path)
+            # Extract objects from images using XML data
+            objs_from_img = extract_objects_from_img(img_path, objs_from_xml)
+            # Save extracted objects to images
+            save_objects_to_img(objs_from_img, UNSORTED_DIR)
+            # Move raw image to processed directory
+            if move_images:
+                for f in [img_path, xml_path]:
+                    new_path = f.replace(raw_images_dir, PROCESSED_DIR)
+                    print(f"    Moving {f} to {new_path}")
+                    rename(f, new_path)
+        else:
+            print(f"    No XML file found for {img_path}")
+
+# %%
