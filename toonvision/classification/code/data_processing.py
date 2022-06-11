a073b0c4629387d351d10685d6b7127855abb7f6
Sason B <nosas@users.noreply.github.com>
2022-06-10 23:16:58 -0700

    Add recursive argument to glob

--- toonvision/classification/code/data_processing.py
+++ toonvision/classification/code/data_processing.py
@@ -63,26 +63,27 @@ def process_images(
     move_images: bool = False,
 ) -> None:
     """Extract objects from raw images and save them to the unsorted img directory"""
-    screenshots = glob(f"{raw_images_dir}/*.{image_type}", recursive=True)
-    print(f"Found {len(screenshots)} screenshots")
+    screenshots = glob(f"{raw_images_dir}/**/.{image_type}", recursive=True)
+    print(f"Found {len(screenshots)} screenshots in {raw_images_dir}")
     for img_path in screenshots:
-        print(f"Processing {img_path}")
-        xml_path = img_path.replace(f".{image_type}", ".xml")
-        if path.exists(xml_path):
-            # Extract objects' labels and bounding box dimensions from XML
-            objs_from_xml = extract_objects_from_xml(xml_path)
-            # Extract objects from images using XML data
-            objs_from_img = extract_objects_from_img(img_path, objs_from_xml)
-            # Save extracted objects to images, modify image name to include object index
-            save_objects_to_img(objs_from_img, UNSORTED_DIR)
-            # Move raw image to processed directory
-            if move_images:
-                for f in [img_path, xml_path]:
-                    new_path = f.replace(raw_images_dir, PROCESSED_DIR)
-                    print(f"    Moving {f} to {new_path}")
-                    rename(f, new_path)
-        else:
-            print(f"    No XML file found for {img_path}")
+        if "ttr-screenshot-Fri-Jun-10" in img_path:
+            print(f"Processing {img_path}")
+            xml_path = img_path.replace(f".{image_type}", ".xml")
+            if path.exists(xml_path):
+                # Extract objects' labels and bounding box dimensions from XML
+                objs_from_xml = extract_objects_from_xml(xml_path)
+                # Extract objects from images using XML data
+                objs_from_img = extract_objects_from_img(img_path, objs_from_xml)
+                # Save extracted objects to images, modify image name to include object index
+                save_objects_to_img(objs_from_img, UNSORTED_DIR)
+                # Move raw image to processed directory
+                if move_images:
+                    for f in [img_path, xml_path]:
+                        new_path = f.replace(raw_images_dir, PROCESSED_DIR)
+                        print(f"    Moving {f} to {new_path}")
+                        rename(f, new_path)
+            else:
+                print(f"    No XML file found for {img_path}")
 
 
 def split_data(dry_run: bool = False):
@@ -132,4 +133,6 @@ def unsort_data(dry_run: bool = False):
 # # %% Unsort data
 # unsort_data(dry_run=True)
 
+# %%
+process_images()
 # %%
