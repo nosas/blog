d8d54a8ba6724c0087bff95ff0bf54de411afa25
Sason B <nosas@users.noreply.github.com>
2022-06-11 23:54:03 -0700

    Reformat main.py to follow article

--- toonvision/classification/code/main.py
+++ toonvision/classification/code/main.py
@@ -1,45 +1,55 @@
 # %% Imports
+from glob import glob
+
 import keras
-from tensorflow.keras.utils import image_dataset_from_directory
+import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
+from keras import layers
+from tensorflow.keras.utils import image_dataset_from_directory
 
 from data_processing import (
     DATA_DIR,
+    SCREENSHOTS_DIR,
     TEST_DIR,
     TRAIN_DIR,
+    UNSORTED_DIR,
     VALIDATE_DIR,
     process_images,
     split_data,
-    SCREENSHOTS_DIR,
-    UNSORTED_DIR,
 )
 from data_visualization import (
+    compare_histories,
+    plot_datasets_all,
+    plot_datasets_animals,
+    plot_datasets_binary,
+    plot_datasets_suits,
     plot_history,
     plot_image_sizes,
     plot_suits_as_bar,
     plot_toons_as_bar,
     plot_xml_data,
-    compare_histories,
 )
-from model_utils import make_model
+from model_utils import make_model, predict_image
 
 # %% Convert all images in screenshots directory to data images
-process_images(raw_images_dir=SCREENSHOTS_DIR + "/br", move_images=False)
-
-# %% Plot bar of suits
-# plot_suits_as_bar()
-
-# # %% Plot bar of toons
-# plot_toons_as_bar()
-
-# # %% Plot xml data
-# plot_xml_data()
-
-# # %% Plot all image sizes in unsorted directory
-# plot_image_sizes(TRAIN_DIR)
+process_images(raw_images_dir=SCREENSHOTS_DIR, move_images=False, filename_filter="Fri-Jun-10")
+process_images(raw_images_dir=SCREENSHOTS_DIR, move_images=False, filename_filter="Sat-Jun-11")
 
 # %% Split unsorted images into train, validate, and test sets
-# from data_processing import split_data
+# split_data()
+
+# %% Plot bar of suits
+plot_suits_as_bar(img_dir=DATA_DIR)
+
+# # %% Plot bar of toons
+plot_toons_as_bar(img_dir=DATA_DIR)
+
+# # %% Plot xml data
+plot_xml_data()
+
+# # %% Plot all image sizes in unsorted directory
+plot_image_sizes(TRAIN_DIR)
 
 
 # %% Read images into keras dataset
@@ -59,37 +69,31 @@ test_dataset = image_dataset_from_directory(
     # batch_size=16
 )
 
+
+# %% Plot the balance of the datasets
+plot_datasets_suits()
+plot_datasets_animals()
+plot_datasets_binary()
+plot_datasets_all()
+
 # %% Define data augmentations
-# data_augmentation = keras.Sequential(
-#     [
-#         # Apply horizontal flipping to 50% of the images
-#         layers.RandomFlip("horizontal"),
-#         # Rotate the input image by some factor in range [-10%, 10%] or [-36, 36] in degrees
-#         layers.RandomRotation(0.1),
-#         # Zoom in or out by a random factor in range [-20%, 20%]
-#         layers.RandomZoom(0.2),
-#     ]
-# )
-
-# %% Create models
-# model = make_model(name="toonvision")
-# model_augmentation = make_model(
-#     name="toonvision_augmentation", augmentation=data_augmentation
-# )
-# model_augmentation_dropout = make_model(
-#     name="toonvision_augmentation_dropout", augmentation=data_augmentation, dropout=0.5
-# )
-# all_models = [model, model_augmentation, model_augmentation_dropout]
-
-# %% View model summary
-# all_models[0].summary()
+data_augmentation = keras.Sequential(
+    [
+        # Apply horizontal flipping to 50% of the images
+        layers.RandomFlip("horizontal"),
+        # Rotate the input image by some factor in range [-20%, 20%] or [-72, 72] in degrees
+        layers.RandomRotation(0.2),
+        # Zoom in or out by a random factor in range [-30%, 30%]
+        layers.RandomZoom(0.3),
+    ]
+)
 
 # %% Compile all models
 learning_rate = 0.0001
 optimizers = [
-    tf.keras.optimizers.SGD(learning_rate=learning_rate * 100),
-    tf.keras.optimizers.Adam(learning_rate=learning_rate),
-    tf.keras.optimizers.RMSprop(learning_rate=learning_rate),
+    # tf.keras.optimizers.SGD(learning_rate=learning_rate * 100),
+    # tf.keras.optimizers.Adam(learning_rate=learning_rate),
+    # tf.keras.optimizers.RMSprop(learning_rate=learning_rate),
     # tf.keras.optimizers.Adadelta(learning_rate=learning_rate),
     # tf.keras.optimizers.Adagrad(learning_rate=learning_rate),
     # tf.keras.optimizers.Adamax(learning_rate=learning_rate),
@@ -99,11 +103,22 @@ optimizers = [
 models_all = []
 for opt in optimizers:
     model = make_model(name="toonvision_" + opt._name)
-    model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
-    models_all.append(model)
-# optimizer = keras.optimizers.adam_v2.Adam(learning_rate=0.0001)
-# for model in all_models:
-#     model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+    model_augmentation = make_model(
+        name=f"toonvision_{opt._name}_augmentation", augmentation=data_augmentation
+    )
+    model_augmentation_dropout = make_model(
+        name=f"toonvision_{opt._name}_augmentation_dropout",
+        augmentation=data_augmentation,
+        dropout=0.75,
+    )
+
+    models = [model, model_augmentation, model_augmentation_dropout]
+    for model in models:
+        model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
+        models_all.append(model)
+
+# %% Print model summary
+print(model.summary())
 
 # %% Train model
 histories_all = []  # List of tuples: tuple[model, history]
@@ -123,8 +138,8 @@ for model in models_all:
     histories_all.append((model, history))
 
 # %% Plot training and validation loss
-# for model, history in all_histories:
-#     plot_history(history=history.history, name=model.name)
+for model, history in histories_all:
+    plot_history(history=history.history, name=model.name)
 
 # %% Test model
 for model in models_all:
@@ -133,42 +148,50 @@ for model in models_all:
     print(f"{model.name} Test Loss: {test_loss:.2f}")
     print(f"{model.name} Test Accuracy: {test_accuracy:.2f}")
 
-# %% Predict on unseen images
-# TODO: Add code to predict on unseen images
-import matplotlib.pyplot as plt
-import numpy as np
-from glob import glob
-
-c = "../img/unsorted/cog/cog_sb_namedropper_0.png"
-t = "../img/unsorted/toon/toon_bear_0.png"
-model = keras.models.load_model("toonvision_Nadam.keras")
-
-
-def predict_image(filename: str, model: tf.keras.Model) -> str:
-    img = keras.preprocessing.image.load_img(filename, target_size=(600, 200))
-    ia = keras.preprocessing.image.img_to_array(img)
-    # plt.imshow(ia/255.)
-    ia = np.expand_dims(ia, axis=0)
-    classes = model.predict(ia)
-    label = "Toon" if classes[0] > 0.5 else "Cog"
-    # print(f"{label} : {filename}")
-    return (label, classes)
-
-
-for fn in glob(f"{DATA_DIR}/**/cog/*.png", recursive=True):
-    label, pred = predict_image(fn, models_all[3])
-    if label == "Toon":
-        print(f"{fn}, {label} {pred}")
-
-
-for fn in glob(f"{DATA_DIR}/**/toon/*.png", recursive=True):
-    label, pred = predict_image(fn, models_all[3])
-    if label == "Cog":
-        print(f"{fn}, {label} {pred}")
-
-
 # %% Compare training histories for all optimizers
 compare_histories(histories_all)
+
+# %% Predict on unseen images
+preds_wrong = []
+for model in models_all:
+    model = keras.models.load_model(f"{model.name}.keras")
+    preds = []
+
+    for fn in glob(f"{DATA_DIR}/**/cog/*.png", recursive=True):
+        label, pred = predict_image(fn, model)
+        if label == "Toon":
+            print(f"{fn}, {label} {pred}")
+            preds.append((fn, label, pred, abs(pred - 0.5)))
+
+    for fn in glob(f"{DATA_DIR}/**/toon/*.png", recursive=True):
+        label, pred = predict_image(fn, model)
+        if label == "Cog":
+            print(f"{fn}, {label} {pred}")
+            preds.append((fn, label, pred, abs(pred - 0.5)))
+
+    preds_wrong.append((model.name, preds))
+
+# %% Plot the wrong predictions by highest error rate (most wrong)
+for (model_name, pred_wrong) in preds_wrong:
+    wrong = np.array(pred_wrong)
+    wrong = wrong[wrong[:, 3].argsort()]
+    wrong = wrong[::-1]
+
+    # %% Plot the wong predictions by highest error rate (most wrong)
+    plt.figure(figsize=(15, 15))
+    for i in range(len(wrong[:15])):
+        plt.subplot(3, 5, i + 1)
+        plt.imshow(
+            keras.preprocessing.image.load_img(wrong[i][0], target_size=(600, 200))
+        )
+        label = wrong[i][1]
+        error = f"{wrong[i][3][0][0]:.2f}"
+        accuracy = f"{wrong[i][2][0][0]:.2f}"
+        plt.title(f"{label}\n(E:{error}, A:{accuracy})")
+        plt.axis("off")
+    plt.suptitle(f"Wrong predictions {len(wrong)} {model_name}")
+    plt.tight_layout()
+    plt.show()
 
 # %% Remove optimizers with vanishing gradients
 vanishing_gradients = ["Adadelta", "Adagrad", "Ftrl", "SGD"]
