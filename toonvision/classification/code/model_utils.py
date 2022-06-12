275f592b86d5ca663ad90b316ea4b2d76b46bc9a
Sason B <nosas@users.noreply.github.com>
2022-06-11 23:52:01 -0700

    Add  function to model_utils

--- toonvision/classification/code/model_utils.py
+++ toonvision/classification/code/model_utils.py
@@ -1,5 +1,6 @@
 # %% Imports and functions
 import keras
+import numpy as np
 
 # from tensorflow import keras
 from keras import layers
@@ -12,20 +13,35 @@ def make_model(
     if augmentation:
         x = augmentation(inputs)
     x = layers.Rescaling(1.0 / 255)(inputs)
-    x = layers.Conv2D(filters=16, kernel_size=3, activation="relu")(x)
     x = layers.MaxPooling2D(pool_size=2)(x)
     x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
-    x = layers.MaxPooling2D(pool_size=2)(x)
     x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
     x = layers.MaxPooling2D(pool_size=2)(x)
     x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
     x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
     x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
     x = layers.MaxPooling2D(pool_size=2)(x)
     x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
     x = layers.Flatten()(x)
     if dropout:
         x = layers.Dropout(dropout)(x)
     outputs = layers.Dense(units=1, activation="sigmoid")(x)
     model = keras.Model(name=name, inputs=inputs, outputs=outputs)
     return model
+
+
+def predict_image(filename: str, model: keras.Model) -> str:
+    img = keras.preprocessing.image.load_img(filename, target_size=(600, 200))
+    ia = keras.preprocessing.image.img_to_array(img)
+    # plt.imshow(ia/255.)
+    ia = np.expand_dims(ia, axis=0)
+    classes = model.predict(ia)
+    label = "Toon" if classes[0] > 0.5 else "Cog"
+    # print(f"{label} : {filename}")
+    return (label, classes)
+
+# %%
