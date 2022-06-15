95e6b3f0a57c3df2ff77bb187286bc7c8af13653
Sason B <nosas@users.noreply.github.com>
2022-06-14 23:50:22 -0700

    Create smaller, more effective model architecture

--- toonvision/classification/code/model_utils.py
+++ toonvision/classification/code/model_utils.py
@@ -34,6 +34,28 @@ def make_model(
     return model
 
 
+def make_model_optimized(
+    name: str = "", augmentation: keras.Sequential = None, dropout: float = 0.0
+) -> keras.Model:
+    inputs = keras.Input(shape=(600, 200, 3))
+    if augmentation:
+        x = augmentation(inputs)
+    x = layers.Rescaling(1.0 / 255)(inputs)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=16, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Flatten()(x)
+    if dropout:
+        x = layers.Dropout(dropout)(x)
+    outputs = layers.Dense(units=1, activation="sigmoid")(x)
+    model = keras.Model(name=name, inputs=inputs, outputs=outputs)
+    return model
+
+
 def predict_image(filename: str, model: keras.Model) -> str:
     img = keras.preprocessing.image.load_img(filename, target_size=(600, 200))
     ia = keras.preprocessing.image.img_to_array(img)
