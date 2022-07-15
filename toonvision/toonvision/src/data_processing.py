b180f6fb6133afdc35b87c262626799297f7f2c0
Sason B <nosas@users.noreply.github.com>
2022-07-14 19:48:26 -0700

    Reduce image processing's verbosity

--- toonvision/toonvision/src/data_processing.py
+++ toonvision/toonvision/src/data_processing.py
@@ -63,13 +63,15 @@ def process_images(
     image_type: str = "png",
     move_images: bool = False,
     filename_filter: str = "",
+    verbose: bool = False,
 ) -> None:
     """Extract objects from raw images and save them to the unsorted img directory"""
     screenshots = glob(f"{raw_images_dir}/**/*.{image_type}", recursive=True)
     print(f"Found {len(screenshots)} screenshots in {raw_images_dir}")
     for img_path in screenshots:
         if filename_filter in img_path:  # TODO move to glob above
-            print(f"Processing {img_path}")
+            if verbose:
+                print(f"Processing {img_path}")
             xml_path = img_path.replace(f".{image_type}", ".xml")
             if path.exists(xml_path):
                 # Extract objects' labels and bounding box dimensions from XML
@@ -77,12 +79,13 @@ def process_images(
                 # Extract objects from images using XML data
                 objs_from_img = extract_objects_from_img(img_path, objs_from_xml)
                 # Save extracted objects to images, modify image name to include object index
-                save_objects_to_img(objs_from_img, UNSORTED_DIR, DATA_DIR)
+                save_objects_to_img(objs_from_img, UNSORTED_DIR, DATA_DIR, verbose=verbose)
                 # Move raw image to processed directory
                 if move_images:
                     for f in [img_path, xml_path]:
                         new_path = f.replace(raw_images_dir, PROCESSED_DIR)
-                        print(f"    Moving {f} to {new_path}")
+                        if verbose:
+                            print(f"    Moving {f} to {new_path}")
                         rename(f, new_path)
             else:
                 print(f"    No XML file found for {img_path}")
@@ -127,7 +130,7 @@ def create_datasets(
     shuffle: bool = True,
     split_ratio: list[float, float, float] = None,
 ):
-    """Create binary datasets for training, validation, and testing
+    """Create binary (Cog vs Toon) datasets for training, validation, and testing
 
     Args:
         image_size (tuple, optional): Tuple of height and width. Defaults to (600, 200).
@@ -160,6 +163,47 @@ def create_datasets(
         shuffle=shuffle,
     )
     return (ds_train, ds_validate, ds_test)
+
+
+# def create_suit_datasets(
+#     image_size: tuple = (600, 200),
+#     batch_size: int = 32,
+#     shuffle: bool = True,
+#     split_ratio: list[float, float, float] = None,
+# ):
+#     """Create multiclass Cog suit datasets for training, validation, and testing
+
+#     Args:
+#         image_size (tuple, optional): Tuple of height and width. Defaults to (600, 200).
+#         batch_size (int, optional): Number of samples per batch. Defaults to 32.
+#         shuffle (bool, optional): Shuffle images in dataset. Defaults to True.
+#         split_ratio (list[float, float, float], optional): Train/val/test split. Defaults to None.
+
+#     Returns:
+#         tuple[keras.Dataset, keras.Dataset, keras.Dataset]: Train, validate, and test datasets.
+#     """
+#     if split_ratio:
+#         split_data(split_ratio=split_ratio, dry_run=False)
+
+#     # ds_train = image_dataset_from_directory(
+#     #     TRAIN_DIR + "/cog",
+#     #     image_size=image_size,
+#     #     batch_size=batch_size,
+#     #     shuffle=shuffle,
+#     # )
+#     # ds_validate = image_dataset_from_directory(
+#     #     VALIDATE_DIR + "/cog",
+#     #     image_size=image_size,
+#     #     batch_size=batch_size,
+#     #     shuffle=shuffle,
+#     # )
+#     # ds_test = image_dataset_from_directory(
+#     #     TEST_DIR + "/cog",
+#     #     image_size=image_size,
+#     #     batch_size=batch_size,
+#     #     shuffle=shuffle,
+#     # )
+#     return (ds_train, ds_validate, ds_test)
 
 
 def unsort_data(dry_run: bool = False):
