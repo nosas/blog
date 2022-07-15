1df2e69369fdbc1d9dce7c1ff83bdfe36862479c
Sason B <nosas@users.noreply.github.com>
2022-07-14 19:50:37 -0700

    Create initial multiclass model

--- toonvision/toonvision/src/model_utils.py
+++ toonvision/toonvision/src/model_utils.py
@@ -10,6 +10,35 @@ from keras import layers
 from data_processing import DATA_DIR, create_datasets, unsort_data
 
 
+def make_multiclass_model_original(
+    name: str = "",
+    augmentation: keras.Sequential = None,
+    dropout: float = 0.0,
+) -> keras.Model:
+    inputs = keras.Input(shape=(600, 200, 3))
+    if augmentation:
+        x = augmentation(inputs)
+    x = layers.Rescaling(1.0 / 255)(inputs)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=16, kernel_size=3, activation="relu")(x)
+    x = layers.Conv2D(filters=16, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+    x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Flatten()(x)
+    if dropout:
+        x = layers.Dropout(dropout)(x)
+    outputs = layers.Dense(units=4, activation="softmax")(x)
+    model = keras.Model(name=name, inputs=inputs, outputs=outputs)
+    return model
+
+
 def make_model_padding(
     name: str = "",
     augmentation: keras.Sequential = None,
