ef650c48c91c5bf45eb40e9deece472aefee2499
Sason B <nosas@users.noreply.github.com>
2022-08-13 14:36:29 -0700

    Extract plotting methods

--- performance_classification/code/main.py
+++ performance_classification/code/main.py
@@ -37,34 +37,7 @@ def calc_f1_score(x, y) -> float:
     return 2 * (x * y) / (x + y)
 
 
-def plot_means(precision, recall, ax=None, title: str = "Harmonic and Arithmetic Means"):
-    if not ax:
-        fig = plt.figure(figsize=(5, 5))
-        ax = fig.add_subplot(111)
-
-    # Add a title
-    ax.set_title(title)
-    arithmetic = calc_mean_arithmetic(precision, recall)
-    f1 = calc_f1_score(precision, recall)
-    harmonic = f1 / 2
-
-    # Add horizontal grid lines to the plot
-    ax.grid(True, which="major", axis="y", linestyle="-", alpha=0.4)
-
-    # Set [p, r] as the x-axis
-    ax.set_xticks([1, 2])
-    ax.set_xticklabels(["precision", "recall"])
-
-    # Set the y-axis to 5
-    ax.set_ylim(0, 5)
-    # Set the x-axis to 2
-    ax.set_xlim(0.5, 2.5)
-
-    # Plot precisions[0] in first column
-    ax.plot(1, precision, "o", label="Precision", markersize=10, color=COLORS[0])
-    # Plot precisions[1] in second column
-    ax.plot(2, recall, "o", label="Recall", markersize=10, color=COLORS[1])
-
+def plot_arithmetic(precision: float, recall: float, ax, arithmetic: float):
     # Plot the arithmetic mean
     # Plot a line from the precision marker to the recall marker
     ax.plot(
@@ -72,15 +45,26 @@ def plot_means(precision, recall, ax=None, title: str = "Harmonic and Arithmetic
         [precision, recall],
         "--",
         linewidth=2.5,
-        alpha=0.7,
+        alpha=0.25,
         zorder=0,
         color=COLORS[2],
     )
     # Plot a dot in the middle of the line
     ax.plot(1.5, arithmetic, "*", label="Arithmetic", markersize=10, color=COLORS[2])
-    # Plot a vertical line from 1.5 to arithmetic
-    ax.plot([1.5, 1.5], [0, arithmetic], "--", linewidth=2.5, alpha=0.5, zorder=0, color=COLORS[2])
+    # Add annotations arithmetic mean
+    ax.annotate(
+        f"{arithmetic:.2f}",
+        xy=(1.5, arithmetic),
+        xytext=(1.5 + 0.1, arithmetic + 0.2),
+        ha="center",
+        size=12,
+        color=COLORS[2],
+    )
 
+
+def plot_f1(
+    precision: float, recall: float, ax, arithmetic: float, f1: float, harmonic: float
+):
     # Plot the harmonic mean
     # Declare the x, y coordinates for each lines' start and end points
     l1_x = [1, 2]
@@ -96,65 +80,88 @@ def plot_means(precision, recall, ax=None, title: str = "Harmonic and Arithmetic
     int_x = (l2_b - l1_b) / (l1_m - l2_m)
     int_y = f1 / 2  # int_y = l1_m * int_x + l1_b
     # Plot a line from the bottom of the precision bar to the top of the recall bar
-    ax.plot(l1_x, l1_y, "-", linewidth=2.5, alpha=0.5, zorder=0, color=COLORS[3])
+    ax.plot(l1_x, l1_y, "-", linewidth=2.5, alpha=0.25, zorder=0, color=COLORS[3])
     # Plot a line from the bottom of the precision bar to the top of the recall bar
-    ax.plot(l2_x, l2_y, "-", linewidth=2.5, alpha=0.5, zorder=0, color=COLORS[3])
+    ax.plot(l2_x, l2_y, "-", linewidth=2.5, alpha=0.25, zorder=0, color=COLORS[3])
     # Plot a dot at the intersection of the two lines
     ax.plot(int_x, int_y, "*", label="Harmonic", markersize=10, color=COLORS[3])
     ax.plot(int_x, int_y * 2, "*", label="F1", markersize=10, color=COLORS[6])
-    # Plot a vertical line from the x-axis to the intersection point
-    ax.plot(  # Intersection point
-        [int_x, int_x],
-        [0, int_y],
-        "--",
-        linewidth=2.5,
-        alpha=0.5,
-        zorder=0,
-        color=COLORS[3],
-    )
-    # Plot a vertical line from the x-axis to the F1-score
-    ax.plot(  # F1-score
-        [int_x, int_x],
-        [int_y, int_y * 2],
-        "--",
-        linewidth=2.5,
-        alpha=0.5,
-        zorder=0,
-    )
-    # Add annotations to the arithmetic mean, harmonic mean, and F1-score
-    ax.annotate(
-        f"{arithmetic:.2f}",
-        xy=(1.5, arithmetic),
-        xytext=(1.5 + 0.1, arithmetic + 0.1),
-        ha="center",
-        size=14,
-        color=COLORS[2],
-    )
+    # Add annotations to the harmonic mean and F1-score
     ax.annotate(
         f"{harmonic:.2f}",
         xy=(int_x, int_y),
-        xytext=(int_x + 0.15, int_y - 0.075),
+        xytext=(int_x + 0.2, int_y - 0.1),
         ha="center",
-        size=14,
+        size=12,
         color=COLORS[3],
     )
-    if f1 != arithmetic:
+    if f1 != arithmetic:  # Prevent duplicate annotation
         ax.annotate(
             f"{f1:.2f}",
             xy=(int_x, int_y * 2),
-            xytext=(int_x + 0.15, int_y * 2),
+            xytext=(int_x + 0.2, int_y * 2),
             ha="center",
-            size=14,
+            size=12,
             color=COLORS[6],
         )
+
+
+def plot_means(
+    precision: float,
+    recall: float,
+    ax=None,
+    title: str = "Harmonic and Arithmetic Means",
+):
+    if not ax:
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.add_subplot(111)
+
+    # Calculate the arithmetic mean, harmonic mean, and F1-score
+    arithmetic = calc_mean_arithmetic(precision, recall)
+    f1 = calc_f1_score(precision, recall)
+    harmonic = f1 / 2
+
+    # Add a title
+    ax.set_title(title)
+    # Add horizontal grid lines to the plot
+    ax.grid(True, which="major", axis="y", linestyle="-", alpha=0.25)
+    # Set [p, r] as the x-axis
+    ax.set_xticks([1, 2])
+    ax.set_xticklabels(["precision", "recall"])
+    # Set the y-axis to 5
+    ax.set_ylim(0, 5)
+    # Set the x-axis to 2
+    ax.set_xlim(0.5, 2.5)
+
+    # Plot precision in first column
+    ax.plot(1, precision, "o", label="Precision", markersize=10, color=COLORS[0])
+    ax.axvline(1, ymax=precision / 5, color=COLORS[0])
+    # Plot recall in second column
+    ax.plot(2, recall, "o", label="Recall", markersize=10, color=COLORS[1])
+    ax.axvline(2, ymax=recall / 5, color=COLORS[1])
+
+    plot_arithmetic(precision, recall, ax, arithmetic)
+    plot_f1(precision, recall, ax, arithmetic, f1, harmonic)
     # Add a legend
     ax.legend()
-
 
 
 # %% Initialize variables
 precisions = [3, 1, 2]
 recalls = [2, 4.5, 2]
+titles = ["Mild variance", "High variance", "Equal scores"]
+
+# %% Plot the figures
+fig, ax = plt.subplots(1, 3, figsize=(12, 5), dpi=100, sharey=True)
+fig.suptitle("Arithmetic vs F1-score\n", size=16)
+for axis, precision, recall, title in zip(ax, precisions, recalls, titles):
+    plot_means(precision, recall, axis, title)
+
+for axis in ax[:2]:
+    # Remove the legend
+    axis.legend_.remove()
+
+fig.tight_layout()
 
 # %% Calculate means
 scores_mean = np.array(
@@ -173,9 +180,5 @@ Harmonic  : {scores_f1}
 Difference: {scores_mean - scores_f1}
 """
 )
-
-# %%
-for precision, recall in zip(precisions, recalls):
-    plot_means(precision, recall)
 
 # %%
