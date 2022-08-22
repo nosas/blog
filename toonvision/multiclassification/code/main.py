0296acf9660742b834b2e126b80fcb4388fd1e85
Sason B <nosas@users.noreply.github.com>
2022-08-21 21:57:34 -0700

    Create function: Confidence levels, image grid
    TOOD: Move to the respective library files

--- toonvision/multiclassification/code/main.py
+++ toonvision/multiclassification/code/main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from data_processing import (
+    SUITS_LONG,
     SUITS_SHORT,
     TEST_DIR,
     TRAIN_DIR,
@@ -28,7 +29,11 @@ from data_visualization import (
 )
 from img_utils import get_image_augmentations
 from keras_tuner import BayesianOptimization, Hyperband, RandomSearch
-from model_utils import make_multiclass_model_original, make_multiclass_model_padding
+from model_utils import (
+    make_multiclass_model_original,
+    make_multiclass_model_padding,
+    make_multiclass_model_tuned,
+)
 from sklearn.metrics import (
     classification_report,
     f1_score,
@@ -667,8 +672,8 @@ def model_builder_hyperband_final(hp):
     )
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+        loss=LOSS,
+        metrics=METRICS,
     )
     return model
 
@@ -679,9 +684,9 @@ tuner = Hyperband(
     hypermodel=model_builder_hyperband_final,
     objective="val_loss",
     max_epochs=100,
-    executions_per_trial=3,
+    executions_per_trial=2,
     directory="models",
-    project_name="tuned_multiclass_hyperband_final",
+    project_name="tuned_multiclass_hyperband_final2",
     seed=SEED,
 )
 tuner.search_space_summary()
@@ -705,7 +710,7 @@ tuner.search(
 # %%
 # model = tuner.get_best_models(num_models=1)[0]
 # model.build(input_shape=(None, 600, 200, 3))
-params = tuner.get_best_hyperparameters(num_trials=7)[4]
+params = tuner.get_best_hyperparameters(num_trials=10)[0]
 model = tuner.hypermodel.build(params)
 params.values
 
@@ -735,17 +740,19 @@ plot_history(
     multiclass=True,
     loss_ylim=(0, 1),
     includes_validation=False,
+    onehot=ONEHOT,
 )
 
-# %% Plot the wrong predictions
-model = keras.models.load_model("./models/tuned_hyperband2_tv_Adam.keras")
+# Plot the wrong predictions
+# model = keras.models.load_model("./models/tuned_hyperband2_tv_Adam.keras")
 
 predictions = model.predict(test_images)
 preds_int = np.asarray([np.argmax(p) for p in predictions], dtype=np.int32)
-preds_str = label_decoder(preds_int)
+preds_str = integer_to_suit(preds_int)
 
 # Get the wrong predictions as a True/False array
-mask = preds_int.astype("int") != test_labels.astype("int")
+# mask = np.argwhere(tf.keras.metrics.categorical_accuracy(test_labels, predictions) == 0)
+mask = preds_str != label_decoder(test_labels)
 wrong_idxs = np.argwhere(mask).transpose().flatten()
 
 wrong_fps = [test_fps[i] for i in wrong_idxs]
@@ -765,10 +772,10 @@ for model_fp in glob("./models/*.keras"):
     # model.summary()
     predictions = model.predict(test_images)
     preds_int = np.asarray([np.argmax(p) for p in predictions], dtype=np.int32)
-    preds_str = label_decoder(preds_int)
+    preds_str = integer_to_suit(preds_int)
 
     # Get the wrong predictions as a True/False array
-    mask = preds_int.astype("int") != test_labels.astype("int")
+    mask = preds_str != label_decoder(test_labels)
     wrong_idxs = np.argwhere(mask).transpose().flatten()
 
     wrong_fps = [test_fps[i] for i in wrong_idxs]
@@ -795,6 +802,192 @@ for model_fp in glob("./models/*.keras"):
         title=f"All Predictions: {model.name}",
     )
 
+# %% Plot the wrong predictions
+model = keras.models.load_model("./models/tuned_hyperband_final_tv_Adam.keras")
+# model = make_multiclass_model_tuned(name="toonvision_multiclass_tuned")
+# model.compile(
+#     loss=LOSS,
+#     optimizer=tf.keras.optimizers.Adam(1e-3),
+#     metrics=METRICS
+# )
+# print(f"Training model {model.name}")
+# history = model.fit(
+#     np.concatenate((train_images, val_images)),
+#     np.concatenate((train_labels, val_labels)),
+#     epochs=250,
+#     # batch_size=BATCH_SIZE,
+#     # shuffle=True,
+#     verbose=0,
+#     callbacks=[
+#         tf.keras.callbacks.EarlyStopping(
+#             monitor="loss", patience=15, restore_best_weights=True
+#         ),
+#     ],
+# )
+# plot_history(
+#     history=history.history,
+#     name=model.name,
+#     multiclass=True,
+#     loss_ylim=(0, 1),
+#     includes_validation=False,
+#     onehot=ONEHOT,
+# )
+# # Evaluate the model against the test set
+# print(model.evaluate(test_images, test_labels, batch_size=BATCH_SIZE))
+
+predictions = model.predict(test_images)
+preds_int = np.asarray([np.argmax(p) for p in predictions], dtype=np.int32)
+preds_str = integer_to_suit(preds_int)
+
+# Get the wrong predictions as a True/False array
+# mask = np.argwhere(tf.keras.metrics.categorical_accuracy(test_labels, predictions) == 0)
+mask = preds_str != label_decoder(test_labels)
+wrong_idxs = np.argwhere(mask).transpose().flatten()
+
+wrong_fps = [test_fps[i] for i in wrong_idxs]
+wrong_preds = [preds_str[i] for i in wrong_idxs]
+wrong_actual = label_decoder(test_labels[i] for i in wrong_idxs)
+wrong = list(zip(wrong_fps, wrong_preds, wrong_actual))
+
+plot_wrong_predictions_multiclass(
+    wrong, model_name="toonvision_multiclass_tuned", show_num_wrong=2
+)
+print(
+    classification_report(
+        y_true=label_decoder(test_labels), y_pred=preds_str, labels=SUITS_SHORT
+    )
+)
+plot_confusion_matrix(
+    predictions=wrong_preds,
+    targets=wrong_actual,
+    display_labels=SUITS_SHORT,
+    title=f"Wrong Predictions: {model.name}",
+)
+plot_confusion_matrix(
+    predictions=preds_str,
+    targets=label_decoder(test_labels),
+    display_labels=SUITS_SHORT,
+    title=f"All Predictions: {model.name}",
+)
 # %% Plot confidence values for each wrong prediction
 
 # %% Plot the top 10 least confident predictions
+# Get the indices of the 10 least confident predictions
+lc_preds_values = np.amax(predictions, axis=1)
+lc_preds_idxs = np.argsort(lc_preds_values)
+lc10_preds_idxs = lc_preds_idxs[:10]
+# sorted(lc_preds_values)[:10] == np.amax(predictions[lc10_preds_idxs], axis=1)
+# Get the corresponding filepath, prediction
+lc10_fps = test_fps[lc10_preds_idxs]
+lc10_fps_short = [fp.split("\\")[-1].strip(".png") for fp in lc10_fps]
+lc10_preds = predictions[lc10_preds_idxs]
+
+# %% Create function to visualize prediction confidence as a horizontal, stacked bar chart
+# https://matplotlib.org/stable/gallery/lines_bars_and_markers/horizontal_barchart_distribution.html
+category_names = SUITS_LONG
+
+
+def survey(
+    results,
+    category_names,
+    category_colors: list[str] = None,
+    title: str = "Confidence levels for predictions",
+):
+    """
+    Parameters
+    ----------
+    results : dict
+        A mapping from question labels to a list of answers per category.
+        It is assumed all lists contain the same number of entries and that
+        it matches the length of *category_names*.
+    category_names : list of str
+        The category labels.
+    """
+    labels = list(results.keys())
+    data = np.array(np.array(list(results.values())) * 100).astype("int")
+    data_cum = data.cumsum(axis=1)
+    if not category_colors:
+        category_colors = plt.colormaps["Set1"](np.linspace(0.15, 0.85, data.shape[1]))
+
+    fig, ax = plt.subplots(figsize=(8, 4), dpi=100)
+    ax.invert_yaxis()
+    ax.xaxis.set_visible(False)
+    ax.set_xlim(0, np.sum(data, axis=1).max())
+
+    for i, (colname, color) in enumerate(zip(category_names, category_colors)):
+        widths = data[:, i]
+        starts = data_cum[:, i] - widths
+        rects = ax.barh(
+            labels, widths, left=starts, height=0.5, label=colname, color=color
+        )
+
+        # r, g, b, _ = color
+        # text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
+        text_color = "white"
+        # labels = [str(int(w*100)) if w >= 0.01 else "" for w in widths]
+        for rect in rects:
+            if rect.get_width() > 0:
+                ax.text(
+                    rect.get_x() + rect.get_width() / 2,
+                    rect.get_y() + rect.get_height() / 2,
+                    str(int(rect.get_width())),
+                    ha="center",
+                    va="center",
+                    color=text_color,
+                    fontsize=10,
+                )
+
+        # ax.bar_label(container=rects, label_type='center', color=text_color)
+    ax.legend(
+        ncol=len(category_names),
+        bbox_to_anchor=(0.05, 1),
+        loc="lower left",
+        fontsize="large",
+    )
+
+    # Set title
+    ax.set_title(f"{title}\n\n", fontsize=12)
+
+    return fig, ax
+
+
+# %% Plot the least confident predictions
+results = {lc10_fps_short[i]: lc10_preds[i] for i in range(len(lc10_fps_short))}
+colors = ["brown", "darkblue", "darkgreen", "maroon"]
+
+fig, ax = survey(results, category_names, colors, title="Confidence levels for least confident predictions")
+plt.show()
+
+
+# %% Plot confidence intervals for wrong predictions
+wrong_fps_short = [fp.split("\\")[-1].strip(".png") for fp in wrong_fps]
+results = {
+    wrong_fps_short[i]: predictions[wrong_idxs][i] for i in range(len(wrong_preds))
+}
+
+fig, ax = survey(results, category_names, colors, title="Confidence levels for wrong predictions")
+plt.show()
+
+# %% Create image grid given image filepaths
+def create_image_grid(filepaths, ncols=4, title: str = ""):
+    """
+    Create a grid of images given a list of filepaths.
+    """
+    nrows = int(np.ceil(len(filepaths) / ncols))
+    fig, ax = plt.subplots(nrows, ncols, figsize=(ncols * 2.5, nrows * 3.25), dpi=100)
+    for i, fp in enumerate(filepaths):
+        if nrows == 1:
+            ax[i].imshow(tf.keras.utils.load_img(fp))
+            ax[i].axis("off")
+        else:
+            ax[int(i / ncols), i % ncols].imshow(tf.keras.utils.load_img(fp))
+            ax[int(i / ncols), i % ncols].axis("off")
+
+    # Add the title
+    fig.suptitle(title, fontsize=16)
+    fig.tight_layout()
+    return fig, ax
+
+create_image_grid(lc10_fps[:5], ncols=5, title="Least confident samples")
+
+# %%
